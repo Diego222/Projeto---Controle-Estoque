7ead99a7852c418d94375114d87c92dbafd2107d
Diego de Camargo Alvarenga <diegodecamargoalvarenga@ub-aqa-46.w3s.local>
2014-10-14 13:59:35 -0300

    Tela Login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :usuarios
   resources :pedidos
 
   resources :produtos
@@ -16,7 +17,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root :to => "clientes#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
