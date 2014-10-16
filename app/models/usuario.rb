4f82b8b8f26b2d51f8f6cfe117ea66b2ccc47a18
Diego de Camargo Alvarenga <diegodecamargoalvarenga@ub-aqa-46.w3s.local>
2014-10-16 15:13:45 -0300

    Layout

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -3,4 +3,6 @@ class Usuario < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  #validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{3,})\z/i, on: :create, :message => "Email inválido" }
 end
